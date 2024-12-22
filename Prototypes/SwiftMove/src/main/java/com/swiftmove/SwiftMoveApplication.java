87982e1a9134276526167a00bb0fe96eae8f6a6f
Parth Gajjar <147626010+ghostp13409@users.noreply.github.com>
2024-12-22 18:16:02 -0500

    Initialize Project

---
+++ Prototypes/SwiftMove/src/main/java/com/swiftmove/SwiftMoveApplication.java
@@ -0,0 +1,13 @@
+package com.swiftmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftMoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftMoveApplication.class, args);
+    }
+
+}
