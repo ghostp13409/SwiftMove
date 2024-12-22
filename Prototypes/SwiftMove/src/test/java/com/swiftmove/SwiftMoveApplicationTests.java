21284d2788ddc6f186676621833df693d2d6c21e
Parth Gajjar <147626010+ghostp13409@users.noreply.github.com>
2024-12-22 18:16:02 -0500

    Initialize Project

---
+++ Prototypes/SwiftMove/src/test/java/com/swiftmove/SwiftMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftMoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
