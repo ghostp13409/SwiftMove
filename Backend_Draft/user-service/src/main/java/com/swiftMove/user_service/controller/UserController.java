632b35b36bfb71829d342337857f258d3be50938
AnnlinGeorge <annlinpg@gmail.com>
2026-02-20 05:12:08 -0500

    Successfully Registered and routed with Eureka

---
+++ Backend_Draft/user-service/src/main/java/com/swiftMove/user_service/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.swiftMove.user_service.controller;
+
+
+import com.swiftMove.user_service.dto.UserRequestDTO;
+import com.swiftMove.user_service.dto.UserResponseDTO;
+import com.swiftMove.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users/")
+@RequiredArgsConstructor
+public class UserController {
+    @Autowired
+    private final UserService userService;
+
+  @GetMapping("/allUsers")
+    public ResponseEntity<List<UserResponseDTO>> getAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/iam/profile/{id}")
+    public ResponseEntity<UserResponseDTO> getById(@PathVariable Long id){
+        UserResponseDTO userResponseDTO = userService.findById(id);
+        if (userResponseDTO == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+ @PostMapping("/addUser")
+    public ResponseEntity<UserResponseDTO> addNewUser(@RequestBody UserRequestDTO userRequestDTO){
+
+      UserResponseDTO newUserResponseDto=userService.addNewUser(userRequestDTO);
+      return ResponseEntity.status(HttpStatus.CREATED).body(newUserResponseDto);
+    }
+
+    @PutMapping("/iam/profile/{id}")
+    public ResponseEntity<Void> updateUser( @PathVariable Long id, @RequestBody UserRequestDTO updateUser){
+      userService.updateExistingUser(id, updateUser);
+      return ResponseEntity.noContent().build();
+
+    }
+
+}
---
+++ Backend_Draft/user-service/src/main/java/com/swiftMove/user_service/mapper/UserMapper.java
@@ -0,0 +1,52 @@
+package com.swiftMove.user_service.mapper;
+
+import com.swiftMove.user_service.dto.UserRequestDTO;
+//import com.swiftMove.user_service.dto.UpdateUserDTO;
+import com.swiftMove.user_service.dto.UserResponseDTO;
+import com.swiftMove.user_service.model.User;
+
+public class UserMapper {
+
+    // connecting User -> Response Dto
+    public static UserResponseDTO userToResponseDto(User user) {
+        return new UserResponseDTO(
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getDob(),
+                user.getRating(),
+                user.getRole(),
+                user.getAddressId(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+
+    // connecting RequestDTO->User
+    public static User createUserFromDto(UserRequestDTO dto){
+        User user = new User();
+        user.setUsername(dto.getUserName());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setDob(dto.getDob());
+        user.setAddressId(dto.getAddressId());
+
+        return user;
+
+    }
+    // connecting RequestDTO -> user(existing user)
+
+    public static void updateEntity(User existing, UserRequestDTO dto) {
+
+        existing.setUsername(dto.getUserName());
+        existing.setFirstName(dto.getFirstName());
+        existing.setLastName(dto.getLastName());
+        existing.setEmail(dto.getEmail());
+        existing.setDob(dto.getDob());
+        existing.setAddressId(dto.getAddressId());
+    }
+
+
+}
