caa10fe1ce416afa158631ec7825f0dd4e1bc945
AnnlinGeorge <annlinpg@gmail.com>
2026-02-20 15:08:20 -0500

    Add clientManagement-service

---
+++ Backend_Draft/clientManagement-service/src/main/java/com/swiftMove/clientManagement_service/ClientManagementServiceApplication.java
@@ -0,0 +1,17 @@
+package com.swiftMove.clientManagement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ClientManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagementServiceApplication.class, args);
+	}
+
+}
