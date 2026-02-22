a3c99234617f2d953e26a5bf694af2cf68059133
Parth Gajjar <147626010+ghostp13409@users.noreply.github.com>
2026-02-22 12:47:05 -0500

    fix db

--- src/backend/user-service/src/main/java/com/swiftmove/userservice/UserServiceApplication.java
+++ src/backend/user-service/src/main/java/com/swiftmove/userservice/UserServiceApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class UserServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
