3b9f8bf044f001b42f5f6770b9bce5a5f01fae07
Parth Gajjar <gajjarparth09@gmail.com>
2026-03-03 13:15:12 -0500

    fixed db scripts

--- src/backend/api-gateway/src/main/java/com/swiftmove/apigateway/ApiGatewayApplication.java
+++ src/backend/api-gateway/src/main/java/com/swiftmove/apigateway/ApiGatewayApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
