660353076610072a4d609c8d911561798ab83ab8
AnnlinGeorge <annlinpg@gmail.com>
2026-03-08 15:01:41 -0400

    Completed trip-service

---
+++ src/backend/trip-service/src/main/java/com/swiftmove/tripservice/controller/TripController.java
@@ -0,0 +1,49 @@
+package com.swiftmove.tripservice.controller;
+
+import com.swiftmove.tripservice.dto.CreateMoveTripDto;
+import com.swiftmove.tripservice.dto.MoveTripDto;
+import com.swiftmove.tripservice.service.MoveTripService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/trips")
+@RequiredArgsConstructor
+class TripController {
+
+    private final MoveTripService moveTripService;
+
+    // For Admin: GET /trips - get all trips
+    @GetMapping
+    public ResponseEntity<List<MoveTripDto>> getAll()
+    {
+        List<MoveTripDto> trips = moveTripService.getAll();
+        return ResponseEntity.ok(trips);
+    }
+    //Get By ID
+    @GetMapping("/{id}")
+    public ResponseEntity<MoveTripDto> getById(@PathVariable Long id)
+    {
+        MoveTripDto trip = moveTripService.getById(id);
+        if (trip == null)
+            return ResponseEntity.notFound().build();
+      return  ResponseEntity.ok(moveTripService.getById(id));
+    }
+    //Create a new Move Trip
+    @PostMapping
+    public ResponseEntity<MoveTripDto> create(@RequestBody CreateMoveTripDto newMoveTrip)
+    {
+        try {
+            MoveTripDto createdTrip = moveTripService.add(newMoveTrip);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdTrip);
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+}
